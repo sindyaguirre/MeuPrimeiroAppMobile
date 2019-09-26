3dda405cae0076b84697aa6bf5bb83979fdb7d7f
Sindy Antunes <sindy.antunes@hammerconsult.com.br>
2019-09-26 19:25:34 -0300

    Primeiro commit de uma applicação feita em xamarin, passo a passo da Aula no Alura (Xamarin parte 1.)

---
+++ App6/App6/Views/DetalheView.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App6.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetalheView : ContentPage    {
+        private const int FREIO_ABS = 800;
+        private const int ARCONDICIONADO = 1000;
+        private const int MP3_PLAYER = 500;
+
+        bool temFreioABS;
+        bool temArCondicionado;
+        bool temMp3Player;
+
+
+        public Veiculo Veiculo { get; set; }
+
+        public string textoFreioABS{
+            get {
+                return string.Format("Freio ABS R$ {0}",FREIO_ABS);
+                }
+        }
+        //contrutor do detalhe do veiculo
+        public string textoArCondicionado
+        {
+            get
+            {
+                return string.Format("Ar condicionado R$ {0}", ARCONDICIONADO);
+            }
+        }
+
+        public string textoMp3Player
+        {
+            get
+            {
+                return string.Format("MP3 Player R$ {0}", MP3_PLAYER);
+            }
+        }
+
+        public bool TemFreioABS {
+
+            get {
+                return temFreioABS;
+            }
+            set {
+                temFreioABS = value;
+
+                OnPropertyChanged();//notifica quando o botao é alterado valor
+                OnPropertyChanged(nameof(ValorTotal));
+            }
+        }
+        public bool TemArCondicionado{
+            get{
+                return temArCondicionado;
+            }
+            set{
+                temArCondicionado = value;
+
+                OnPropertyChanged();//notifica quando o botao é alterado valor
+                OnPropertyChanged(nameof(ValorTotal));
+            }
+        }
+        public bool TemMp3Player{
+            get
+            {
+                return temMp3Player;
+            }
+            set
+            {
+                temMp3Player = value;
+
+                OnPropertyChanged();//notifica quando o botao é alterado valor
+                OnPropertyChanged(nameof(ValorTotal));
+            }
+        }
+
+        public string ValorTotal {
+            get {
+
+                return string.Format("Valor Total: R$ {0}", 
+                    Veiculo.Preco
+                    +(TemFreioABS ? FREIO_ABS : 0 )
+                    + (TemArCondicionado ? ARCONDICIONADO: 0)
+                    + (TemMp3Player ? MP3_PLAYER: 0)
+                    ) ;
+
+            }
+        }
+
+        public DetalheView(Veiculo veiculo) {
+            InitializeComponent();
+           
+            this.Veiculo = veiculo;
+            this.BindingContext = this;
+        }
+
+        private void ButtonProximo_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new AgendamentoView(this.Veiculo));
+        }
+    }
+}
---
+++ App6/App6/Views/ListagemView.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App6.Views
+{
+
+    public class Veiculo{
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public string PrecoFormatado
+        {
+            get { return string.Format("R$ {0}", Preco); }
+        }
+
+    }
+
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class ListagemView : ContentPage
+    {
+        public List<Veiculo> Veiculos { get; set; }
+        public ListagemView()
+        {
+            InitializeComponent();
+            this.Veiculos = new List<Veiculo>
+            {
+                new Veiculo{ Nome = "Azera V6", Preco=60000},
+                new Veiculo{ Nome = "Fiesta 2.0", Preco=50000},
+                new Veiculo{ Nome = "HB20 S", Preco=40000},
+            };
+            this.BindingContext = this;
+        }
+
+        private void ListViewVeiculos_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var veiculo = (Veiculo) e.Item;
+            //aqui ele navega para  aproxima pagina
+            //Navigation empilha as paginas,ou seja a logica de pilha, e usando  ometdo assincrono
+            //para que esta ação funcione deve ser alterado o arquvo de App.xaml.cs
+            Navigation.PushAsync(new DetalheView(veiculo));
+
+        }
+    }
+}
